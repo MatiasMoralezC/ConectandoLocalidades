d556ceb0cd0fa8776b1836551f710d8b352a1a4f
maceratomas <maceratomas@gmail.com>
2023-05-03 11:07:50 -0300

    Main agregado en arbol generador minimo

--- src/main/java/conectandoLocalidades/agm/ArbolGeneradoMinimo.java
+++ src/main/java/conectandoLocalidades/agm/ArbolGeneradoMinimo.java
@@ -52,4 +52,15 @@ public class ArbolGeneradoMinimo {
             System.out.println(padre[i] + " - " + i + "\t" + grafo[i][padre[i]]);
         }
     }
+    
+    public static void main(String[] args) {
+        int[][] grafo = { { 0, 2, 0, 6, 0 },
+                          { 2, 0, 3, 8, 5 },
+                          { 0, 3, 0, 0, 7 },
+                          { 6, 8, 0, 0, 9 },
+                          { 0, 5, 7, 9, 0 } };
+
+        prim(grafo);
+    }
+    
 }
